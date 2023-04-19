0a4763146dce286757f1e2cdc2fb1d8163eaacf2
Vladislav Bordiug <vladislav.bordioug@gmail.com>
2023-04-19 13:20:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 from deep_translator import GoogleTranslator
   
 import os
-from flask import Flask, request, abort, render_template
-app = Flask(__name__)
-from threading import Thread
-import time
+#from flask import Flask, request, abort, render_template
+#app = Flask(__name__)
 import psycopg2
 from chatgpt import Chatgpt
 from stablediffusion import StableDiffusion
@@ -491,7 +489,7 @@ if __name__ == '__main__':
     db_object = db_connection.cursor()
     application = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).read_timeout(100).get_updates_read_timeout(100).build()
     crypto = AioCryptoPay(token=os.getenv("CRYPTOPAY_KEY"), network=Networks.MAIN_NET)
-    Thread(app.run(port=os.getenv("WEBHOOK_PORT")))
+    #app.run(port=os.getenv("WEBHOOK_PORT"))
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start),MessageHandler(filters.Regex('^🔙Back$'), start)],
         states={
