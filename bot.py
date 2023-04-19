aa8613f0ee42a85259d9e310b67496c38e6fc1b7
Vladislav <vladislav.bordioug@gmail.com>
2023-04-19 12:18:28 +0300

    webhook

--- bot.py
+++ bot.py
@@ -508,7 +508,7 @@ if __name__ == '__main__':
     db_object = db_connection.cursor()
     application = Application.builder().token(os.getenv("TELEGRAM_BOT_TOKEN")).read_timeout(100).get_updates_read_timeout(100).build()
     crypto = AioCryptoPay(token=os.getenv("CRYPTOPAY_KEY"), network=Networks.MAIN_NET)
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host = os.getenv("WEBHOOK_HOST"),port=os.getenv("WEBHOOK_PORT"))
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start),MessageHandler(filters.Regex('^🔙Back$'), start)],
         states={
